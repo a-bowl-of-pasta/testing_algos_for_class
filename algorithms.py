62f5e2f091d410898996dbd2edf2ab801dd9d17b
Anthony Hernandez <ahernandez94476@gmail.com>
2024-09-22 01:48:51 -0500

    sorts V 0.0
    this is the initial commit for the 'Testing_Sorting_Algorithms' project

---
+++ algorithms.py
@@ -0,0 +1,77 @@
+import random as rand
+
+# ======= bubble sort algorithm ======
+def bubbleSort(container):
+
+    for i in container:
+        temp = 0
+        # ===== this is the loop that does the swapping ==== 
+        for j in range(i + 1,len(container) -1): 
+            if i > container[j] :
+                temp = i 
+                i = container[j]
+                container[j] = temp 
+
+    return container 
+                     
+
+# ===== merge sort algorithm ===== 
+def merge(left, right):
+    sortedList = []
+    i = 0
+    j = 0
+    while ( i < len(left) and j < len(right)):
+        if(left[i] < right[j]):
+            sortedList.append(left[i])
+            i += 1
+        
+        elif(right[j] <= left[i]):
+            sortedList.append(right[j])
+            j += 1
+     
+    
+    sortedList.extend(left[i: ])
+    sortedList.extend(right[j: ])
+
+    return sortedList
+
+
+def mergeSort(container):
+    baseCaseVal = len(container)
+
+    if baseCaseVal == 1:
+        return container
+    else : 
+        midpoint = baseCaseVal // 2
+
+        left = mergeSort(container[: midpoint])
+        right = mergeSort(container[midpoint:])
+
+    return merge(left, right)
+
+
+
+
+
+
+# ====== quick sort algorithm ====== 
+def quickSort():
+    return NULL; 
+
+def genNums(startRange = 1, endRange = 100, generatedNums = 15):
+    
+    numbersToBeSorted = []
+    for  i in range(0, 15):
+        aNumber = rand.randint(startRange, endRange)
+        numbersToBeSorted.append(aNumber)
+
+    return numbersToBeSorted
+
+def printResults(unsortedList, sortedList, algorithmUsed):
+    print("the algorithm used was {} ", algorithmUsed)
+    print ("the unsorted list is :: {}", unsortedList)
+    print ("the sorted list is :: {} ", sortedList)
+
+
+if __name__ == "__main__":
+    print("this is the file with the logic ")
---
+++ driverFile.py
@@ -0,0 +1,19 @@
+# oa = our algorithms 
+import algorithms as oa
+
+if __name__ == "__main__":
+
+
+    # ==== TESTING BUBBLE SORT ======
+        
+   
+    # ==== TESTING MERGE SORT======
+
+        unsorted = oa.genNums()
+        sortedList = oa.mergeSort(unsorted)
+        oa.printResults(unsorted, sortedList, "merge sort")
+   
+    # ==== TESTING QUICK SORT ======
+   
+   
+    # ==== TESTING ___ ======
